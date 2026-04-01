3d8b281fca4c6729a60bdeb4c54ea15ccad0c23b
Megan Palmquist <megpalmq@iu.edu>
2026-04-01 11:16:59 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
 import { changePage } from "../model/model.js";
 
+function closeMobileNav() {
+  $("header").removeClass("mobile-nav-open");
+  $(document.body).removeClass("mobile-nav-lock");
+  $(".mobile-nav-toggle").attr("aria-expanded", "false");
+}
+
+function setupMobileNav() {
+  $(document).on("click", ".mobile-nav-toggle", function () {
+    const $header = $("header");
+    const isOpen = $header
+      .toggleClass("mobile-nav-open")
+      .hasClass("mobile-nav-open");
+    $(document.body).toggleClass("mobile-nav-lock", isOpen);
+    $(this).attr("aria-expanded", String(isOpen));
+  });
+
+  // Close menu after selecting any destination.
+  $(document).on("click", "header nav a", function () {
+    closeMobileNav();
+  });
+
+  // Reset menu state when moving back to desktop.
+  $(window).on("resize", function () {
+    if (window.innerWidth > 760) {
+      closeMobileNav();
+    }
+  });
+
+  // Close when user taps outside header.
+  $(document).on("click", function (event) {
+    const isOpen = $("header").hasClass("mobile-nav-open");
+    if (!isOpen) {
+      return;
+    }
+
+    if (!$(event.target).closest("header").length) {
+      closeMobileNav();
+    }
+  });
+
+  // Close with Escape for accessibility.
+  $(document).on("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeMobileNav();
+    }
+  });
+}
+
 function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
@@ -17,6 +65,7 @@ function initSite() {
   }
   // Add is-loaded class to trigger animations
   $(document.body).addClass("is-loaded");
+  setupMobileNav();
 }
 
 // Global scroll listener for about page fade effect
